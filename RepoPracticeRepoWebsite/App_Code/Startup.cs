f47a21bb3aaa7cee4c76286657587d8a4a150092
Col Walang <CWalang1@studentmail.nait.ca>
2015-09-10 16:12:00 -0600

    Initial Project and Solution Setup

---
+++ RepoPracticeRepoWebsite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RepoPracticeRepoWebsite.Startup))]
+namespace RepoPracticeRepoWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
